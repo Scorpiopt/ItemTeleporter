01cba1080f656132385ea229ad9d2e84b383ec98
Taranchuk <nfhfyxer@gmail.com>
2022-05-10 12:19:59 +0300

    done item hauling

--- 1.3/Source/ItemTeleporter/ItemTeleporter/Building_ItemTeleporter.cs
+++ 1.3/Source/ItemTeleporter/ItemTeleporter/Building_ItemTeleporter.cs
@@ -5,8 +5,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine.UIElements;
 using Verse;
 using Verse.AI;
+using Verse.Noise;
 
 namespace ItemTeleporter
 {
@@ -19,26 +21,66 @@ namespace ItemTeleporter
         }
     }
 
+    [HarmonyPatch(typeof(GenSpawn), "Spawn", new System.Type[] { typeof(Thing), typeof(IntVec3), typeof(Map), typeof(Rot4), typeof(WipeMode), typeof(bool) })]
+    public static class GenSpawn_Spawn_Patch
+    {
+        public static void Postfix(Thing __result, bool respawningAfterLoad)
+        {
+            if (Building_ItemTeleporter.buildings.TryGetValue(__result.Map, out var list))
+            {
+                if (__result is Plant || __result is Building || __result is UnfinishedThing)
+                {
+                    return;
+                }
+                if (!__result.Map.reservationManager.IsReservedByAnyoneOf(__result, Faction.OfPlayer) 
+                    && !__result.Map.physicalInteractionReservationManager.IsReserved(__result))
+                {
+                    var storages = list.Where(x => x.compPower.PowerOn).OrderByDescending(x => x.GetStoreSettings().Priority)
+                        .ThenBy(x => x.Position.DistanceTo(__result.Position)).ToList();
+                    if (!__result.Position.GetThingList(__result.Map).Any(x => x is Building_ItemTeleporter))
+                    {
+                        foreach (var storage in storages)
+                        {
+                            if (storage.GetStoreSettings().AllowedToAccept(__result))
+                            {
+                                var goodCells = storage.AllSlotCells().Where(x => StoreUtility.IsGoodStoreCell(x,
+                                    __result.Map, __result, null, Faction.OfPlayer));
+                                if (goodCells.TryRandomElement(out var cell))
+                                {
+                                    __result.Position = cell;
+                                    FleckMaker.ThrowLightningGlow(__result.DrawPos, __result.Map, 0.5f);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     [HarmonyPatch(typeof(Pawn_JobTracker), "StartJob")]
     public static class Pawn_JobTracker_StartJob_Patch
     {
         public static void Prefix(Pawn ___pawn, Job newJob, JobCondition lastJobEndCondition = JobCondition.None, ThinkNode jobGiver = null, bool resumeCurJobAfterwards = false, bool cancelBusyStances = true, ThinkTreeDef thinkTree = null, JobTag? tag = null, bool fromQueue = false, bool canReturnCurJobToPool = false)
         {
-            if (newJob != null && newJob.def == JobDefOf.DoBill)
+            if (newJob != null && newJob.def == JobDefOf.DoBill && Building_ItemTeleporter.buildings.TryGetValue(___pawn.Map, out var list))
             {
-                var storagesAround = GenRadial.RadialDistinctThingsAround(newJob.targetA.Thing.Position, newJob.targetA.Thing.Map, 5, true)
-                    .OfType<Building_ItemTeleporter>().Where(x => x.compPower.PowerOn).ToList();
+                var storagesAround = list.Where(x => x.Position.DistanceTo(newJob.targetA.Thing.Position) <= 5f 
+                    && x.compPower.PowerOn).OrderBy(x => x.Position.DistanceTo(newJob.targetA.Thing.Position)).ToList();
                 if (storagesAround.Any())
                 {
                     for (var i = 0; i < newJob.countQueue.Count; i++)
                     {
-                        if (!newJob.targetQueueB[i].Thing.Position.GetThingList(newJob.targetA.Thing.Map).Any(x => x is Building_ItemTeleporter))
+                        if (!newJob.targetQueueB[i].Thing.Position.GetThingList(newJob.targetA.Thing.Map)
+                            .Any(x => x is Building_ItemTeleporter))
                         {
                             foreach (var storage in storagesAround)
                             {
                                 if (storage.GetStoreSettings().AllowedToAccept(newJob.targetQueueB[i].Thing))
                                 {
-                                    var goodCells = storage.AllSlotCells().Where(x => StoreUtility.IsGoodStoreCell(x, newJob.targetA.Thing.Map, newJob.targetQueueB[i].Thing, ___pawn, ___pawn.Faction));
+                                    var goodCells = storage.AllSlotCells().Where(x => StoreUtility.IsGoodStoreCell(x, 
+                                        newJob.targetA.Thing.Map, newJob.targetQueueB[i].Thing, ___pawn, ___pawn.Faction));
                                     if (goodCells.TryRandomElement(out var cell))
                                     {
                                         var thing = newJob.targetQueueB[i].Thing;
@@ -68,11 +110,36 @@ namespace ItemTeleporter
 
     public class Building_ItemTeleporter : Building_Storage
     {
+
+        public static Dictionary<Map, HashSet<Building_ItemTeleporter>> buildings = new Dictionary<Map, HashSet<Building_ItemTeleporter>>();
         public CompPowerTrader compPower;
         public override void SpawnSetup(Map map, bool respawningAfterLoad)
         {
             base.SpawnSetup(map, respawningAfterLoad);
             compPower = base.GetComp<CompPowerTrader>();
+            if (!buildings.TryGetValue(map, out var list))
+            {
+                buildings[map] = list = new HashSet<Building_ItemTeleporter>();
+            }
+            list.Add(this);
+        }
+
+        public override void DeSpawn(DestroyMode mode = DestroyMode.Vanish)
+        {
+            if (buildings.TryGetValue(this.Map, out var list))
+            {
+                list.Remove(this);
+            }
+            base.DeSpawn(mode);
+        }
+
+        public override void Destroy(DestroyMode mode = DestroyMode.Vanish)
+        {
+            if (buildings.TryGetValue(this.Map, out var list))
+            {
+                list.Remove(this);
+            }
+            base.Destroy(mode);
         }
     }
 }
